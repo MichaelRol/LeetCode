8d207f863a6627d34c7b68ca56f1be3557a9033b
MichaelRol <michael.rollins@hotmail.co.uk>
2021-01-05 22:07:51 +0000

    1. TwoSum - Easy

---
+++ Easy/TwoSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] results = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            int newTarget = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == newTarget) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two values sum to target.");
+    }
+}
+            
