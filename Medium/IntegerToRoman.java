29c1e6026309d3781d1c300f7e7cba3091accc18
MichaelRol <michael.rollins@hotmail.co.uk>
2021-01-19 22:46:50 +0000

    12. Integer to Roman - Medium

---
+++ Medium/IntegerToRoman.java
@@ -0,0 +1,58 @@
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder str = new StringBuilder();
+        while (num >= 1000) {
+            str.append("M");
+            num -= 1000;
+        }
+        if (num >= 900) {
+            str.append("CM");
+            num -= 900;
+        }
+        if (num >= 500) {
+            str.append("D");
+            num -= 500;            
+        }
+        if (num >= 400) {
+            str.append("CD");
+            num -= 400;
+        }
+        while (num >= 100) {
+            str.append("C");
+            num -= 100;
+        }
+        if (num >= 90) {
+            str.append("XC");
+            num -= 90;
+        }
+        if (num >= 50) {
+            str.append("L");
+            num -= 50;
+        }
+        if (num >= 40) {
+            str.append("XL");
+            num -= 40;
+        }
+        while (num >= 10) {
+            str.append("X");
+            num -= 10;
+        }
+        if (num >= 9) {
+            str.append("IX");
+            num -= 9;
+        }
+        if (num >= 5) {
+            str.append("V");
+            num -= 5;
+        }
+        if (num >= 4) {
+            str.append("IV");
+            num -= 4;
+        }
+        while (num >= 1) {
+            str.append("I");
+            num -= 1;
+        }
+        return str.toString();
+    }
+}
