0866b6fef52fa8bca35d0c4ddc144aca3bc84aea
MichaelRol <michael.rollins@hotmail.co.uk>
2021-01-21 20:43:29 +0000

    19. Remove Nth Node from End of List - Medium

---
+++ Medium/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,26 @@
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode n1 = head;
+        ListNode n2 = head;
+        for (int i = 0; i < n; i++) {
+            n1 = n1.next;
+            if (n1 == null) {
+                return head.next;
+            }
+        }
+        while (n1.next != null) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        n2.next = n2.next.next;
+        return head;
+    }
+}
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}      
