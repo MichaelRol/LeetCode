58b846f9b552dac51c39d885f73cded22f397a7a
MichaelRol <michael.rollins@hotmail.co.uk>
2021-01-30 23:54:46 +0000

    21. Merge Two Sorted Lists - Easy

---
+++ Easy/21-MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = new ListNode(l1.val);
+                curr = curr.next;
+                l1 = l1.next;
+            } else {
+                curr.next = new ListNode(l2.val);
+                curr = curr.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            curr.next = l2;
+        } else {
+            curr.next = l1;
+        }
+        return dummy.next;
+    }
+}
+
+ public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
